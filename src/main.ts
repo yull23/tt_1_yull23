227acd7e4ed3b25af0e28ce650e3f23048df29d1
Yull Timoteo <yull.timoteo.z@uni.pe>
2023-11-25 04:02:24 -0500

    Application CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as morgan from 'morgan';
 import { AppModule } from './app.module';
+import { CORS } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.PORT);
-
   app.setGlobalPrefix('api/v1');
+  // app.use(morgan('dev'));
+  app.use(morgan('dev'));
+  app.enableCors(CORS);
+  console.log(CORS);
 
   app.useGlobalPipes(
     new ValidationPipe({
